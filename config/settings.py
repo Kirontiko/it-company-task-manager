aa1df83b0f1e988ac032c9bb1787493494a6292a
Denys Seliverstov <catsperr322@gmail.com>
2023-07-25 18:19:25 +0300

    Modifying secret key in settings file

--- config/settings.py
+++ config/settings.py
@@ -26,9 +26,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY",
-                            "django-insecure-5%a_qapj3q&(u8ik@q74e^vbrn^vhc$_^rwj4q3dl+(-h%e0of"
-                            )
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG") != "False"
